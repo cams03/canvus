13bbf7f9b34daf564146ec8d3c079da2f4a76469
Cams03 <camille.francia@hotmail.fr>
2017-04-24 16:15:29 +0200

    rangement dans le code et les fichiers

--- js/app.js
+++ js/app.js
@@ -1,30 +1,24 @@
 const margin = 20;
 
 function direction(el) {
-
   // DEFINITION DES DIRECTIONS
   // 0:top, 1:right, 2:bottom, 3:left
   const side = Math.floor(Math.random() * 4); // tirage aléatoire d'un nombre entre 0 et 4 qui définira le point de départ
-
   const fromX = Math.floor(Math.random() * (window.innerWidth - 2 * margin)) + margin; //tirage aléatoire d'un nombre entre 20 (margin) et 980 (largeur de la fenêtre - margin) afin de définir une position horizontale
-
   const fromY = Math.floor(Math.random() * (window.innerHeight - 2 * margin)) + margin; //tirage aléatoire d'un nombre entre 20 (margin) et 980 (largeur de la fenêtre - margin) afin de définir une position verticale
 
   if (side === 0) { // top
     el.style.top = 0;
     el.style.left = fromX + "px";
     el.style.animationName = "fly-from-top";
-
   } else if (side === 1) { // right
     el.style.right = 0;
     el.style.top = fromY + "px";
     el.style.animationName = "fly-from-right"
-
   } else if (side === 2) { // bottom
     el.style.bottom = 0;
     el.style.left = fromX + "px";
     el.style.animationName = "fly-from-bottom";
-
   } else if (side === 3) { // left
     el.style.left = 0;
     el.style.top = fromY + "px";
@@ -33,11 +27,8 @@ function direction(el) {
 }
 
 function spawnPlanet() {
-
   // VISIBILITE BON / MAUVAIS
   const elemType = Math.floor(Math.random() * 2); //tirage aléatoire d'un nombre entre 0 et 2 (car 2 éléments 1 bon et 1 mauvais)
-  //  console.log("spawning");
-
   let newPlanet = document.createElement("img"); // création d'un élément image
 
   if (elemType === 0) {
@@ -54,7 +45,6 @@ function spawnPlanet() {
   };
 
   document.getElementById('main').appendChild(newPlanet); //rattache l'objet créé à son parent (#jeu)
-
   direction(newPlanet);
 
   newPlanet.addEventListener('animationend', function() {
@@ -62,31 +52,27 @@ function spawnPlanet() {
   });
 };
 
- let planetTimer;
+function spawnBonus() {
+   const margin = 20;
+   const elemType = Math.floor(Math.random() * 2);
+   let newBonus =document.createElement('img');
 
- function spawnBonus() {
+   if (elemType === 0) {
+     newBonus.src = "pics/chrono.svg";
+     newBonus.className += "chrono";
+     newBonus.style.width = 128 + "px";
+   } else {
+     newBonus.src = "pics/star.svg";
+     newBonus.className += "booster";
+     newBonus.style.width = 128 + "px";
+   }
+   newBonus.className += " fly";
 
-     const margin = 20;
-     const elemType = Math.floor(Math.random() * 2);
-     let newBonus =document.createElement('img');
-
-  if (elemType === 0) {
-    newBonus.src = "pics/chrono.svg";
-    newBonus.className += "chrono";
-    newBonus.style.width = 128 + "px";
-  } else {
-    newBonus.src = "pics/star.svg";
-    newBonus.className += "booster";
-    newBonus.style.width = 128 + "px";
-  }
-  newBonus.className += " fly";
-
-  newBonus.onmouseover = function() {
-    mouseOver(this)
-  };
+   newBonus.onmouseover = function() {
+     mouseOver(this)
+   };
 
   document.getElementById('main').appendChild(newBonus);
-
   direction(newBonus);
 
   newBonus.addEventListener('animationend', function() {
@@ -94,31 +80,26 @@ function spawnPlanet() {
   });
 }
 
- let bonusTimer;
+//CURSEUR & MOUSEOVER
 
- //CURSEUR
-
- window.addEventListener("mousemove", function(evenementmousemove) {
+window.addEventListener("mousemove", function(evenementmousemove) {
      document.getElementById("cursorperso").style.top = evenementmousemove.clientY - 50 + 'px';
      document.getElementById("cursorperso").style.left = evenementmousemove.clientX - 50 + 'px';
- });
+});
 
- function mouseOver(el) {
+function mouseOver(el) {
      if (el.classList.contains("goodplanet")) {
          el.style.display = "none";
          cpt++;
          document.getElementById("baclette").innerHTML = "Score : " + cpt;
-
      } else if (el.classList.contains("chrono")) {
          el.style.display = "none";
          timeLeft = timeLeft + 9;
          timer.innerHTML = timeLeft;
-
      } else if (el.classList.contains("booster")) {
          el.style.display = "none";
          cpt = cpt + 5;
          document.getElementById("baclette").innerHTML = "Score : " + cpt;
-
      } else if (el.classList.contains("badplanet")) {
          if (cpt > 0) {
              el.style.display = "none";
@@ -133,151 +114,100 @@ function spawnPlanet() {
 
  let cpt = 0;
  let timeLeft = 30;
- //my highscore
- let highscore = 0;
+ let highscore = 0;//my highscore
  let timer = document.getElementById("timer");
  let popup = document.getElementById("score");
  let cover = document.getElementById("cover");
  let rejouer = document.getElementById("rejouer");
+ let array = []; //var vide
+ let timerId;
+ let planetTimer;
+ let bonusTimer;
+ let easy = document.getElementById("easy");
+ let medium = document.getElementById("medium");
+ let hard = document.getElementById("hard");
+ let cover2 =document.getElementById("cover2");
+ let levels = document.getElementById("levels");
 
- // compteur de points
- function compteur() {
-     //span où le score est affiché
-     let baclette = document.getElementById("baclette");
- }
-
- //TODO multiple levels
- /*function level() {
-
-   if(!planetTimer) return;
-
-   //planetTimer exists... go ahead
-   if (cpt >= 40) {
-     clearInterval(planetTimer);
-    //  delete planetTimer;
-     planetTimer = setInterval(spawnPlanet, 500);
-     console.log(1,planetTimer);
-   } else if (cpt >= 20) {
-     clearInterval(planetTimer);
-    //  delete planetTimer;
-     planetTimer = setInterval(spawnPlanet, 700);
-     console.log(2,planetTimer);
-   } else if (cpt >= 5) {
-     clearInterval(planetTimer);
-    //  delete planetTimer;
-     planetTimer = setInterval(spawnPlanet, 1000);
-     console.log(3,planetTimer);
-   }
-
- }
-level();*/
-//var vide
-let array = [];
- //Fenetre score final + chrono
+ // FENETRE SCORE FINAL + CHRONO
  function countdown() {
-     //si le temps est �coul�
-     if (timeLeft < 0) {
+     if (timeLeft < 0) { //si le temps est Écoulé
          //clearInterval suspend le timer,le bonus et les planètes
          clearInterval(timerId);
          clearInterval(planetTimer);
          clearInterval(bonusTimer);
-         //ranger tous les points dans un tableau
-             //stocker la valeur cpt
-             let results = array.push(cpt);
-             //faire une loop for pour ajouter les points dans le tableau
-             for(let i = 0;i < results.length; i++) {
+         //ranger tous les points dans un tableau et stocker la valeur cpt
+         let results = array.push(cpt);
+         for(let i = 0;i < results.length; i++) { //faire une loop for pour ajouter les points dans le tableau
                console.log('score n°', i, ' : ', cpt);
-             }
-             console.log(array);
-
+          }
+          console.log(array);
          /*Math.max => prendre le plus grand nombre dans le tableau
          .apply => pour l'appliquer dans un tableau sans répéter toutes les valeurs en détail du tableau array
          (null, array) => Si au moins un des arguments ne peut pas être converti en un nombre, le résultat sera NaN donc le null(représente la nullité au sens où aucune valeur pour l'objet n'est présente).*/
-             //c'est la fenêtre modale qui apparaît à la fin
-             let paragraph = document.getElementById("result");
-             //si le cpt (cad les points) est inférieur à 20
-             if (cpt < 20) {
-               paragraph.innerHTML = "Ton score est de : " + cpt + ". La honte sur toi Maurice !!" + " Highscore :" + Math.max.apply(null, array) + " .";
-             } else if (cpt < 80) {
-               paragraph.innerHTML = "Mouais ton score est de : " + cpt + ". Tu peux faire mieux mon vieux !" + "High score: " + Math.max.apply(null, array) + " .";
-             } else {
-               paragraph.innerHTML = "T'es un killer !" + " Ton score est de " + cpt + ". High score : " + Math.max.apply(null, array) + " .";
-             }
-         //va sur le h1 pour introduire une phrase selon les points reçus
+         let paragraph = document.getElementById("result");//paragraphe qui affiche le résultat à la fin
+         if (cpt < 20) {//si le cpt (cad les points) est inférieur à 20
+              paragraph.innerHTML = "Ton score est de : " + cpt + ". La honte sur toi Maurice !!" + " Highscore :" + Math.max.apply(null, array) + " .";
+         } else if (cpt < 80) {
+              paragraph.innerHTML = "Mouais ton score est de : " + cpt + ". Tu peux faire mieux mon vieux !" + "High score: " + Math.max.apply(null, array) + " .";
+         } else {
+              paragraph.innerHTML = "T'es un killer !" + " Ton score est de " + cpt + ". High score : " + Math.max.apply(null, array) + " .";
+         }
          let gameOver = document.getElementById("over");
-         gameOver.appendChild(paragraph);
-         //classList pour l'ajouter dans les id cover et score
-         cover.classList.add("visible");
-         popup.classList.add("visible");
-         //dans le localStorage, les points sera enregistrer
-         localStorage.setItem('highscore', cpt);
+         gameOver.appendChild(paragraph); // rattache le paragraphe "result" au h1 "over"
+         cover.classList.add("visible"); // affiche la fenêtre pop up
+         popup.classList.add("visible"); // affiche la fenêtre pop up
+         localStorage.setItem('highscore', cpt); //dans le localStorage, les points sera enregistrer
 
      } else {
-         //sinon continue à afficher le temps qui reste
-         timer.innerHTML = timeLeft;
+         timer.innerHTML = timeLeft;//sinon continue à afficher le temps qui reste
          timeLeft--;
     }
  };
 
- //cr�ation d'une variable pour mettre une intervalle réguliere de chaque seconde
- let timerId;
+// CHOIX DU NIVEAU
+ document.addEventListener("DOMContentLoaded", function() {
+     cover2.classList.add("visible");
+     levels.classList.add("visible");
+ })
 
- //add event listener pour rejouer et recommencer à zéro
- rejouer.addEventListener("click", function() {
+ easy.addEventListener("click", function(){
+     cover2.classList.remove("visible");
+     levels.classList.remove("visible");
+     timerId = setInterval(countdown, 1000);
+     planetTimer=setInterval(spawnPlanet, 1000);
+     bonusTimer=setInterval(spawnBonus, 9000);
+ })
+ medium.addEventListener("click", function(){
+     cover2.classList.remove("visible");
+     levels.classList.remove("visible");
+     timerId = setInterval(countdown, 1000);
+     planetTimer=setInterval(spawnPlanet, 700);
+     bonusTimer=setInterval(spawnBonus, 9000);
+ })
+ hard.addEventListener("click", function(){
+     cover2.classList.remove("visible");
+     levels.classList.remove("visible");
+     timerId = setInterval(countdown, 1000);
+     planetTimer=setInterval(spawnPlanet, 500);
+     bonusTimer=setInterval(spawnBonus, 9000);
+ })
+
+rejouer.addEventListener("click", function() { //add event listener pour rejouer et recommencer à zéro
      cover.classList.remove("visible");
      popup.classList.remove("visible");
      cpt = 0;
      timeLeft = 30;
      cover2.classList.add("visible");
      levels.classList.add("visible");
-
- });
-
-let easy = document.getElementById("easy");
-let medium = document.getElementById("medium");
-let hard = document.getElementById("hard");
-let cover2 =document.getElementById("cover2");
-let levels = document.getElementById("levels");
-
-document.addEventListener("DOMContentLoaded", function() {
-    cover2.classList.add("visible");
-    levels.classList.add("visible");
-
-})
-
-easy.addEventListener("click", function(){
-    cover2.classList.remove("visible");
-    levels.classList.remove("visible");
-    timerId = setInterval(countdown, 1000);
-    planetTimer=setInterval(spawnPlanet, 1000);
-    bonusTimer=setInterval(spawnBonus, 9000);
-})
-
-medium.addEventListener("click", function(){
-    cover2.classList.remove("visible");
-    levels.classList.remove("visible");
-    timerId = setInterval(countdown, 1000);
-    planetTimer=setInterval(spawnPlanet, 700);
-    bonusTimer=setInterval(spawnBonus, 9000);
-})
-
-hard.addEventListener("click", function(){
-    cover2.classList.remove("visible");
-    levels.classList.remove("visible");
-    timerId = setInterval(countdown, 1000);
-    planetTimer=setInterval(spawnPlanet, 500);
-    bonusTimer=setInterval(spawnBonus, 9000);
-})
-
+});
 
  //MUSIC PLAY ON OFF
-
  document.getElementById('mute').addEventListener("click", function() {
      document.getElementById('music').pause();
      document.getElementById('sound').classList.add('visible');
      document.getElementById('mute').classList.remove('visible');
  });
-
  document.getElementById('sound').addEventListener("click", function() {
      document.getElementById('music').play();
      document.getElementById('sound').classList.remove('visible');
